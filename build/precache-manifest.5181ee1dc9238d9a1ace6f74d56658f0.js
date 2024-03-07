67943602bde0dc675a1907c94ddd048e1fbefe9a
Harshal Sunil Mahajan <harshalmahajan173@gmail.com>
2024-03-07 14:55:57 +0000

    Third commit

---
+++ build/precache-manifest.5181ee1dc9238d9a1ace6f74d56658f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e160ef57b06b04e75d3163d2642f8187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97c5c4fa511497575d7d",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "a3af22066b8dcea2f241",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "97c5c4fa511497575d7d",
+    "url": "/static/js/2.35768d2b.chunk.js"
+  },
+  {
+    "revision": "17addde56dada538fe1bc33d341e4ae4",
+    "url": "/static/js/2.35768d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3af22066b8dcea2f241",
+    "url": "/static/js/main.b892cd5a.chunk.js"
+  },
+  {
+    "revision": "c1495edec51ec84df206",
+    "url": "/static/js/runtime-main.9305fcc0.js"
+  }
+]);
